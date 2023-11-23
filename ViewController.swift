a701510643b8ed84825ded2298b7d96a04219d2a
Sai-Mannava <121317962+Sai-Mannava@users.noreply.github.com>
2023-11-22 20:36:47 -0800

    Update ViewController.swift

--- ViewController.swift
+++ ViewController.swift
@@ -33,13 +33,27 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var morePetsSwitch: UISwitch!
     
+    
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    
+       func changeColor() -> UIColor{
+
+              let red = CGFloat.random(in: 0...1)
+              let green = CGFloat.random(in: 0...1)
+              let blue = CGFloat.random(in: 0...1)
+
+              return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+          }
     @IBAction func introduceSelfDidTapped(_ sender: UIButton) {
         // Let's us chose the title we have selected from the segmented control
                 let year = yearSegmentedControl.titleForSegment(at: yearSegmentedControl.selectedSegmentIndex)
 
                 // Creating a variable of type string, that holds an introduction. The introduction interpolates the values from the text fields provided.
                 // Currently we can only present the information in a print statement. However, this lets us verify that our app is printing out what is intended!
-        let introduction = "My name is \(firstNameTextField.text!)\(lastNameTextField.text!) and I attend \(schoolNameTextField.text!). I am currently in my \(year!) year and I own \(numberOfPetsLabel.text!) dogs. It is \(morePetsSwitch.isOn) that I want more pets. My hobbies are \(hobbiesLabel.text!)"
+        let introduction = "My name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I attend \(schoolNameTextField.text!). I am currently in my \(year!) year and I own \(numberOfPetsLabel.text!) dogs. It is \(morePetsSwitch.isOn) that I want more pets. My hobbies are \(hobbiesLabel.text!)"
 
 
                 // Creates the alert where we pass in our message, which our introduction.
